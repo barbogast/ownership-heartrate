cffe409dd08398867dd5690076e8ad2ddd70ed78
Mr. Test <mrtest@example.com>
2023-11-30 11:44:03 +0100

    Change file

--- queries/74fca564-abe4-4767-a2e4-3ab084111dd1/sqlStatement.sql
+++ queries/74fca564-abe4-4767-a2e4-3ab084111dd1/sqlStatement.sql
@@ -4,27 +4,23 @@
 -- Generate a temporary table containing timestamps in a 5 minute interval.
 -- This table gets joined to the actual data and will fill gaps in the 
 -- measurement, so we have a continuous time series.
+-- See https://www.sqlite.org/series.html
 WITH RECURSIVE generate_series(value) AS (
-  SELECT unixepoch('2023-11-06T17:50:00')
+  select(select min(timestamp) - min(timestamp) % (5 * 60) from heartrate)
   UNION ALL
   SELECT value + 60 * 5 FROM generate_series
-   WHERE value + 60 * 5 <= unixepoch('2023-11-15T23:00:00')
+   WHERE value + 60 * 5 <= (select max(timestamp) - max(timestamp) % (5 * 60) from heartrate)
 )
 select 
     round(h.bpm) as bpm,
-    s.value as timestamp,
-    strftime('%Y-%m-%dT%H:%M', s.value, 'unixepoch')
+    s.value as timestamp
 from (
     select 
         avg(bpm) as bpm, 
         -- Timestamp %Y-%m-%dT%H:%M where %M is rounded down to the last 5 minute interval
         -- 2023-11-28T13:09:11 --> 2023-11-28T13:05
         -- 2023-11-28T13:11:11 --> 2023-11-28T13:10
-        unixepoch(strftime('%Y-%m-%dT%H:', timestamp, 'unixepoch') || 
-            printf('%02d', 
-                    strftime('%M', timestamp, 'unixepoch') - strftime('%M', timestamp, 'unixepoch') % 5
-            ) || ':00')
-        as five_minute_time
+        timestamp - timestamp % (5 * 60) as five_minute_time
 
     from heartrate h
     group by five_minute_time
